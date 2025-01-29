a07ace5f912e6f5521a4dd138f66ecaed00a7125
va1holl <pezzatini@proton.me>
2025-01-29 08:28:22 +0200

    Обновил логику логгера(классметод)

--- utils/logger.py
+++ utils/logger.py
@@ -5,7 +5,7 @@ class Logger:
     """Класс логгера. Все логи в папке logs/app.log"""
     LOG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "logs", "app.log")
 
-    @staticmethod
-    def logging(log):
-        with open(Logger.LOG_FILE, 'a', encoding="utf-8") as f:
+    @classmethod
+    def logging(cls, log):
+        with open(cls.LOG_FILE, 'a', encoding="utf-8") as f:
             f.write(log + '\n')
