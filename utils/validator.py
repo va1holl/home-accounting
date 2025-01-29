06cf20fb70eecf9c085865df8ada53723f126ab6
va1holl <pezzatini@proton.me>
2025-01-29 07:09:05 +0200

    Создание валидаторов для ввода в поле и обновление View

---
+++ utils/validator.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+
+class Validator(ABC):
+    """Абстрактный класс для валидаторов"""
+    # Должно быть положительное, целое число
+    @abstractmethod
+    def validate(self, value):
+        pass
+
+
+class EmptyStrValidator(Validator):
+    """Валидатор для проверки пустоты строки"""
+    def validate(self, value):
+        if value is None or value == "":
+            raise ValueError("Ошибка! Поле не должно быть пустым!")
+        return value
+
+
+class IntegerValidator(EmptyStrValidator):
+    """Валидатор для проверки целых чисел"""
+    def validate(self, value):
+        value = super().validate(value)
+        try:
+            return int(value)
+        except ValueError:
+            raise ValueError("Ошибка: Введите целое число.")
+
+
+class PositiveNumberValidator(IntegerValidator):
+    """Валидатор для проверки положительных чисел"""
+    def validate(self, value):
+        number = super().validate(value)
+        if number <= 0:
+            raise ValueError("Ошибка: Введите положительное число.")
+        return number
--- views/view.py
+++ views/view.py
@@ -1,15 +1,23 @@
 import tkinter as tk
 from controllers.controller import Controller
+from utils.validator import IntegerValidator, PositiveNumberValidator
 
 
 class View:
     def __init__(self):
         self.controller = Controller()
+        self.validator = PositiveNumberValidator()
         self.root = tk.Tk()
         self.root.title("Домашняя бухгалтерию")
+        self.x = self.root.winfo_screenwidth()
+        self.y = self.root.winfo_screenheight()
+        self.root.geometry(f'{int(self.x * 0.4)}x{int(self.y * 0.4)}')
 
         self.balance_label = tk.Label(self.root, text=f'Баланс: {self.controller.get_balance()}')
         self.balance_label.pack()
+
+        self.entry_amount = tk.Entry(self.root)
+        self.entry_amount.pack()
 
         self.income_button = tk.Button(self.root, text='Добавить прибыль', command=self.add_income)
         self.income_button.pack()
@@ -21,19 +29,25 @@ class View:
         self.expense_button.pack()
 
     def add_income(self):
-        self.controller.process_income(100, 'Тест_доход_100')
-        self.update_balance()
+        try:
+            self.controller.process_income(PositiveNumberValidator.validate(self.validator, self.entry_amount.get()), 'Тест_доход_100')
+            self.update_balance()
+        except ValueError as e:
+            print(str(e))
 
     def add_expense(self):
         try:
-            self.controller.process_expense(50, 'Тест_расход_50')
+            self.controller.process_expense(PositiveNumberValidator.validate(self.validator, self.entry_amount.get()), 'Тест_расход_50')
             self.update_balance()
         except ValueError as e:
             print(str(e))
 
     def take_credit(self):
-        self.controller.process_credit(20, 'Тест_кредит_20')
-        self.update_balance()
+        try:
+            self.controller.process_credit(PositiveNumberValidator.validate(self.validator, self.entry_amount.get()), 'Тест_кредит_20')
+            self.update_balance()
+        except ValueError as e:
+            print(str(e))
 
     def update_balance(self):
         self.balance_label.config(text=f'Баланс: {self.controller.get_balance()}')
