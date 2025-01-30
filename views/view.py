5cfc09a9d7f1c6d65faabf7231c1d03240068758
va1holl <pezzatini@proton.me>
2025-01-30 20:08:01 +0200

    Синхронизация

--- views/view.py
+++ views/view.py
@@ -1,8 +1,9 @@
 import tkinter as tk
-from wsgiref.validate import validator
-
+from tkinter import ttk
+from utils.loaders import HistoryLoader
 from controllers.controller import Controller
 from utils.validator import PositiveNumberValidator, CommentValidator
+from utils.transaction_formatter import TransactionFormatter
 
 
 class View:
@@ -11,35 +12,45 @@ class View:
         self.controller = Controller()
         self.validator = PositiveNumberValidator()
         self.comm_validator = CommentValidator()
+        self.hl = HistoryLoader()
+        self.formatter = TransactionFormatter()
         self.root = tk.Tk()
         self.root.title("Домашняя бухгалтерию")
         self.x = self.root.winfo_screenwidth()
         self.y = self.root.winfo_screenheight()
-        self.root.geometry(f'{int(self.x * 0.4)}x{int(self.y * 0.4)}')
+        self.root.geometry(f'{int(self.x * 0.6)}x{int(self.y * 0.6)}')
 
-        self.balance_label = tk.Label(self.root, text=f'Баланс: {self.controller.get_balance()}')
+        self.balance_label = ttk.Label(self.root, text=f'Баланс: {self.controller.get_balance()}')
         self.balance_label.pack()
 
-        self.label_entry = tk.Label(self.root, text='Сумма')
+        self.label_entry = ttk.Label(self.root, text='Сумма')
         self.label_entry.pack()
 
-        self.entry_amount = tk.Entry(self.root)
+        self.entry_amount = ttk.Entry(self.root)
         self.entry_amount.pack()
 
-        self.label_entry = tk.Label(self.root, text='Комментарий')
+        self.label_entry = ttk.Label(self.root, text='Комментарий')
         self.label_entry.pack()
 
-        self.entry_comment = tk.Entry(self.root, name='comment')
+        self.entry_comment = ttk.Entry(self.root, name='comment')
         self.entry_comment.pack()
 
-        self.income_button = tk.Button(self.root, text='Добавить прибыль', command=self.add_income)
+        self.income_button = ttk.Button(self.root, text='Добавить прибыль', command=self.add_income)
         self.income_button.pack()
 
-        self.expense_button = tk.Button(self.root, text='Добавить расход', command=self.add_expense)
+        self.expense_button = ttk.Button(self.root, text='Добавить расход', command=self.add_expense)
         self.expense_button.pack()
 
-        self.expense_button = tk.Button(self.root, text='Взять кредит', command=self.take_credit)
+        self.expense_button = ttk.Button(self.root, text='Взять кредит', command=self.take_credit)
         self.expense_button.pack()
+
+        self.transaction_history = tk.Text(self.root, height=5, width=100)
+        self.transaction_history.pack()
+
+        transactions = self.formatter.format(self.hl.load_history())
+
+        for i, trans in enumerate(transactions, start=1):
+            self.transaction_history.insert(f"{i}.0", trans + "\n")
 
     def add_income(self):
         try:
