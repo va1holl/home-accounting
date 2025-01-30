478f8ca2ee4552415aa887275ad47def48017faa
va1holl <pezzatini@proton.me>
2025-01-30 10:06:06 +0200

    Разделил лоадеры(Баланс, история транзакций)

--- utils/loaders.py
+++ utils/loaders.py
@@ -1,13 +1,23 @@
-import os
 from models.database import DataBaseSelect
 
 
-class BalanceLoader:
-    """Класс для загрузки баланса с логов"""
-    def __init__(self):
-        self.db = DataBaseSelect()
+class Loader:
+    db = DataBaseSelect()
 
+
+class BalanceLoader(Loader):
+    """Класс для загрузки баланса с логов"""
     def load_balance(self):
-        data = self.db.select()
+        sql = "SELECT * FROM accounting_logger"
+        data = self.db.select(sql)
         current_balance = int(data[-1][-1])
         return current_balance
+
+
+class HistoryLoader(Loader):
+    """Класс для загрузки последних транзакций"""
+    def load_history(self):
+        sql = "SELECT * FROM accounting_logger"
+        data = self.db.select(sql)
+        history = data[-10:]
+        return history
